7bc884ce26e9e49fbb8d60b5ec3052128eb5d028
Noah Spott <ncs5113@psu.edu>
2023-09-20 19:01:23 -0400

    - Styled Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,8 +3,17 @@ import React from 'react'
 export default function Contact(){
     return(
         <>
-            <h1>Hi There, I Am Noah</h1>
-            <h1>Freelance Developer</h1>
+            <section className='contact-container'>
+
+                <div className='contact-button-section'>
+                    <h1>Hi There, I Am Noah</h1>
+                    <h1 id='contact-text'>Freelance Developer</h1>
+                    <button id='contact-button'>CONTACT</button>
+                </div>
+
+                <img className='circle' src="src/assets/noah-bust.jpg" alt="Noah Photo" />
+
+            </section>
         </>
     )
 }
