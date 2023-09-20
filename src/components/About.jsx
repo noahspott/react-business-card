159fad90aadef863c3574ac2413a7b80766fdf40
Noah Spott <ncs5113@psu.edu>
2023-09-20 14:27:47 -0400

    - Created components - Removed some code from template initialization

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About(){
+    return(
+        <>
+            <h2>TECHNOLOGIES</h2>
+            <h2>EDUCATION</h2>
+        </>
+    )
+}
---
+++ src/components/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Contact(){
+    return(
+        <>
+            <h1>Hi There, I Am Noah</h1>
+            <h1>Freelance Developer</h1>
+        </>
+    )
+}
