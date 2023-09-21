dcc48c1d66a154692e6d27e561a5dc45a01562be
Noah Spott <ncs5113@psu.edu>
2023-09-21 10:50:08 -0400

    - Styled About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 
 export default function About(){
     return(
-        <>
-            <h2>TECHNOLOGIES</h2>
-            <h2>EDUCATION</h2>
-        </>
+        <div id='about-container'>
+            <div className='about-column'>
+                <h2>TECHNOLOGIES</h2>
+                <ul>
+                    <li>JavaScript</li>
+                    <li>HTML5</li>
+                    <li>CSS</li>
+                    <li>React</li>
+                    <li>Bootstrap</li>
+                    <li>Python</li>
+                    <li>Java</li>
+                    <li>C / C++</li>
+                    <li>Git</li>
+                </ul>
+            </div>
+            <div className='about-column'>
+                <h3>EDUCATION</h3>
+                <div>
+                    <h4>B.S. Software Engineering</h4>
+                    <p>The Pennsylvania State University</p>
+                    <p>Class of 2023</p>
+                    <br></br>
+                </div>
+                <div>
+                    <h4>A.A.S. Music Recording Technology</h4>
+                    <p>Luzerne County Community College</p>
+                    <p>Class of 2014</p>
+                </div>
+
+            </div>
+        </div>
     )
 }
