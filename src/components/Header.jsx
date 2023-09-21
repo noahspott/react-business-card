af660b4b07c3c3b8fa15ff09c863cc6e2a85b6cc
Noah Spott <ncs5113@psu.edu>
2023-09-21 10:07:27 -0400

    - Styling header and contact

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Header(){
     return(
         <>
-            <nav className='header--nav'>
-                <h3 className='header--name'>NOAH SPOTT</h3>
+            <nav className='header-nav'>
+                <h3 className='header-name'>NOAH SPOTT</h3>
                 <div id='header-container-logo'>
                     <img className='header-logo' src="src/assets/li-logo.png" alt="LinkedIn Logo" />
                     <img className='header-logo' src="src/assets/github-mark-white.png" alt="GitHub Logo" />
