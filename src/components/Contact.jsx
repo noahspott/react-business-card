5f1953bd447fa6cd5bacac860e552daac05f74c5
Noah Spott <ncs5113@psu.edu>
2023-09-27 15:30:44 -0400

    - Some styling. - Removed contact button.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,9 +7,8 @@ export default function Contact(){
             <section className='contact-container'>
 
                 <div className='contact-button-section'>
-                    <h1>Hi There, I Am Noah</h1>
+                    <h1>Hi there, I'm Noah</h1>
                     <h1 id='contact-text'>Freelance Developer</h1>
-                    <button id='contact-button'>CONTACT</button>
                 </div>
     
                 <img className='circle' src={noahPhoto} alt="Noah Photo" />
